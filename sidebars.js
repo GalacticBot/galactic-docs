87ed6b97906d94597787c625a5f557d4c2ac0e63
Navy <navydotgif@gmail.com>
2020-07-26 11:50:59 +0300

    sidebar config

---
+++ sidebars.js
@@ -0,0 +1,26 @@
+module.exports = {
+  docs: [
+    {
+      type: 'doc',
+      id: 'getting-started'
+    },
+    {
+      type: 'category',
+      label: 'Commands',
+      items: ['commands/admin', 'commands/mod', 'commands/info', 'commands/util', 'commands/misc' ]
+    },
+    {
+      type: 'category',
+      label: 'Settings',
+      items: ['settings/mod', 'settings/util']
+    },
+    {
+      type: 'doc',
+      id: 'premium'
+    }
+  ]
+  // docs: {
+  //   Commands: ['commands/moderation-commands', 'doc2', 'doc3'],
+  //   Features: ['mdx'],
+  // },
+};
